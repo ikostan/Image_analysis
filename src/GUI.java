cc61e8ce8ff8f38efd6f5ff17aab3001976f4876
ikostan <superegorius@gmail.com>
2017-04-30 19:05:59 -0700

    First GUI client draft

---
+++ src/GUI.java
@@ -0,0 +1,103 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class GUI extends JFrame{
+	
+	private JTextField textResult;
+	
+	//Constructor
+	public GUI() {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //X button
+		this.setSize(440,260); //Window size
+		setResizable(false); //Not re-sizable
+		setTitle("Image analysis"); //Title
+		getContentPane().setLayout(null);
+		
+		setLables(); //Create lables
+		setCombos(); //Create combo-boxes
+		setCompareBtn(); //Compare button 
+		setCloseBtn(); //Close button
+		setResetBtn(); //Create Reset button
+		setTxtField(); //Create text field
+	
+		this.setVisible(true);
+	}
+	
+	//Methods
+	
+	//Set labels
+	private void setLables(){
+		
+		JLabel lblSelectImageOne = new JLabel("Select image:");
+		lblSelectImageOne.setBounds(36, 26, 79, 14);
+		getContentPane().add(lblSelectImageOne);
+		
+		JLabel lblSelectImageTwo = new JLabel("Select image:");
+		lblSelectImageTwo.setBounds(280, 26, 79, 14);
+		getContentPane().add(lblSelectImageTwo);
+		
+		JLabel lblVs = new JLabel("VS");
+		lblVs.setBounds(208, 54, 21, 14);
+		getContentPane().add(lblVs);
+	}
+	
+	//Set combo-boxes
+	private void setCombos(){
+		
+		JComboBox comboImageOne = new JComboBox();
+		comboImageOne.setBounds(36, 51, 115, 20);
+		getContentPane().add(comboImageOne);
+		
+		JComboBox comboImageTwo = new JComboBox();
+		comboImageTwo.setBounds(280, 51, 115, 20);
+		getContentPane().add(comboImageTwo);
+	}
+	
+	//Create Compare button
+	private void setCompareBtn(){
+		
+		JButton btnCompare = new JButton("Compare");
+		btnCompare.setBounds(169, 171, 89, 23);
+		getContentPane().add(btnCompare);
+	}
+	
+	//Create Close button
+	private void setCloseBtn(){
+		JButton btnClose = new JButton("Close");
+		//Event handler
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				System.exit(0);
+			}
+		});
+		btnClose.setBounds(306, 171, 89, 23);
+		getContentPane().add(btnClose);
+	}
+	
+	//Create Reset button
+	private void setResetBtn(){
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.setBounds(36, 171, 89, 23);
+		getContentPane().add(btnReset);
+	}
+	
+	//Create text field
+	private void setTxtField(){
+		
+		textResult = new JTextField();
+		textResult.setEditable(false);
+		textResult.setBounds(36, 108, 359, 29);
+		getContentPane().add(textResult);
+		textResult.setColumns(10);
+	}
+	
+	//End
+}
---
+++ src/ImgReader.java
@@ -0,0 +1,13 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class ImgReader {
+
+	//Class fields
+	private String imageSrc;
+	private BufferedImage buffImage;	
+	private Image[] images; 
+	
+	
+	
+}
